14192257b2e52c1dcc70c3abc9c1fa90c77d3405
Yefenny Chireno <yefenny300@gmail.com>
2017-08-21 22:09:54 -0400

    make it better

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  config.assets.unknown_as = false
+  #config.assets.unknown_as = false
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
