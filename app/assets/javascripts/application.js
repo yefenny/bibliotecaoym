566823af573331226326649093fcbe6e434b50cf
Yefenny Chireno <yefenny300@gmail.com>
2017-08-23 04:48:22 -0400

    make it better

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
 //= require twitter/bootstrap
 //= require moment
 //= require turbolinks
-//= require jquery
 //= require jquery_ujs
 //= require jquery.min.js
 //= require jquery-ui
