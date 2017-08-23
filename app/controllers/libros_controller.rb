34f3692d66f104e712a0f0d289994dfb7ff2be5f
Yefenny Chireno <yefenny300@gmail.com>
2017-08-23 18:53:19 -0400

    make it better

--- app/controllers/libros_controller.rb
+++ app/controllers/libros_controller.rb
@@ -31,7 +31,7 @@ class LibrosController < ApplicationController
          check = "0"
          check = params[:libro][:fisico].to_s  
          puts "valorrrrrrrrrrrrrrrrrrrrr" + check  
-            if params[:fisico] != '0'   #VALIDAR SI ES FISICO
+            if check != '0'   #VALIDAR SI ES FISICO
                @libro.fisico = true
             else
                @libro.fisico = false
@@ -46,7 +46,7 @@ class LibrosController < ApplicationController
                @libro.digital = false
                @libro.save
             end  
-            if params[:fisico] != '0' 
+            if check != '0' 
                @cantidad.cantidad = params[:cantidad]
                @cantidad.cantidad_total = params[:cantidad]
                @cantidad.libro_id = @libro.id
