442cad20566d638666333025d9cf34130041a772
Yefenny Chireno <yefenny300@gmail.com>
2017-08-23 15:15:01 -0400

    make it better

--- app/controllers/repdescargas_controller.rb
+++ app/controllers/repdescargas_controller.rb
@@ -1,12 +1,21 @@
 class RepdescargasController < ApplicationController
+before_action :authenticate_user!	
   def index
+  	if current_user.rol_id == 1 || current_user.rol_id == 2 
+  	else
+  		render 'menu/noautorizado'
+    end    	
   end
 
   def create
-  	@fechadesde = params[:fechadesde]
-  	@fechahasta = params[:fechahasta]
-    @libros = Repdescarga.where("fecha >= '#{@fechadesde}' and fecha <= '#{@fechahasta}'").group("title,libro_id,nombre_area,nombre_asignatura").select("title,libro_id,nombre_area,nombre_asignatura, COUNT (libro_id) as cantidad")
-    #@libro = Vbuscalibronombre.joins("left join librodescargas on v_busca_libros_nombre.libro_id = librodescargas.libro_id").group("title,libro_id, nombre_area,nombre_asignatura").select("v_busca_libros_nombre.*, count(librodescargas.id) as cuenta_tweets")
-    render :partial=> 'repdescarga'
+  	if current_user.rol_id == 1 || current_user.rol_id == 2 
+	  	@fechadesde = params[:fechadesde]
+	  	@fechahasta = params[:fechahasta]
+	    @libros = Repdescarga.where("fecha >= '#{@fechadesde}' and fecha <= '#{@fechahasta}'").group("title,libro_id,nombre_area,nombre_asignatura").select("title,libro_id,nombre_area,nombre_asignatura, COUNT (libro_id) as cantidad")
+	    #@libro = Vbuscalibronombre.joins("left join librodescargas on v_busca_libros_nombre.libro_id = librodescargas.libro_id").group("title,libro_id, nombre_area,nombre_asignatura").select("v_busca_libros_nombre.*, count(librodescargas.id) as cuenta_tweets")
+	    render :partial=> 'repdescarga'
+	else
+	   render 'menu/noautorizado'
+    end     
   end
 end
--- app/controllers/repprestamos_controller.rb
+++ app/controllers/repprestamos_controller.rb
@@ -1,36 +1,46 @@
 class RepprestamosController < ApplicationController
+  before_action :authenticate_user!
   def index
-  	@buscarl = "buscar"
-  	@libros = Libro.order("title").all
+    if current_user.rol_id == 1 || current_user.rol_id == 2  
+    	@buscarl = "buscar"
+    	@libros = Libro.order("title").all
+    else
+      render 'menu/noautorizado'
+    end    
   end
   def create
-  	@buscarpor = params[:group1]
-  	@fechadesde = params[:fechadesde].to_date.strftime("%Y-%m-%d")
-  	@fechahasta = params[:fechahasta].to_date.strftime("%Y-%m-%d")
-  	@fechaahora = Time.now.strftime("%Y-%m-%d")
-  	@estudiante_matricula = params[:estudiante_matricula]
-  	@libro_id = params[:libro_id]
-  	sql = ""
-  	if @buscarpor == "activos"
-  		sql +="fechad IS NULL and fechae >= '#{@fechaahora}' "
-  	elsif @buscarpor =="todos"	
-  		sql +="fechap = fechap"
-  	elsif @buscarpor =="devueltos"	
-  		sql +="fechad is not NULL"
-  	elsif @buscarpor =="vencidos"
-  		sql +="fechad IS NULL and fechae < '#{@fechaahora}' "		
-  	end 
-  	if @estudiante_matricula != ""
-  		sql+= " and matricula ilike '#{@estudiante_matricula}'"
-  	end	
-  	if @libro_id != ""
-  		sql+= " and libro_id = #{@libro_id}"
-  	end	
-  	if @fechadesde !="" && @fechahasta !="" && @buscarpor =="todos"
-  		sql+= " and fechap >= '#{@fechadesde}' and fechap <= '#{@fechahasta}'"
-  	end	
+   if current_user.rol_id == 1 || current_user.rol_id == 2   
+    	@buscarpor = params[:group1]
+    	@fechadesde = params[:fechadesde].to_date.strftime("%Y-%m-%d")
+    	@fechahasta = params[:fechahasta].to_date.strftime("%Y-%m-%d")
+    	@fechaahora = Time.now.strftime("%Y-%m-%d")
+    	@estudiante_matricula = params[:estudiante_matricula]
+    	@libro_id = params[:libro_id]
+    	sql = ""
+    	if @buscarpor == "activos"
+    		sql +="fechad IS NULL and fechae >= '#{@fechaahora}' "
+    	elsif @buscarpor =="todos"	
+    		sql +="fechap = fechap"
+    	elsif @buscarpor =="devueltos"	
+    		sql +="fechad is not NULL"
+    	elsif @buscarpor =="vencidos"
+    		sql +="fechad IS NULL and fechae < '#{@fechaahora}' "		
+    	end 
+    	if @estudiante_matricula != ""
+    		sql+= " and matricula ilike '#{@estudiante_matricula}'"
+    	end	
+    	if @libro_id != ""
+    		sql+= " and libro_id = #{@libro_id}"
+    	end	
+    	if @fechadesde !="" && @fechahasta !="" && @buscarpor =="todos"
+    		sql+= " and fechap >= '#{@fechadesde}' and fechap <= '#{@fechahasta}'"
+    	end	
 
-  	@rep = Repprestamo.where(sql).all
-  	render :partial=> 'repprestamo'
+    	@rep = Repprestamo.where(sql).all
+    	render :partial=> 'repprestamo'
+   else
+     render 'menu/noautorizado'
+   end        
+
   end
 end
