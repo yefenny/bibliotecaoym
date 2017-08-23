7e3ff5d858878e42930afe1d6524c558846d77b0
Yefenny Chireno <yefenny300@gmail.com>
2017-08-23 18:05:19 -0400

    make it better

--- app/controllers/repdescargas_controller.rb
+++ app/controllers/repdescargas_controller.rb
@@ -9,8 +9,8 @@ before_action :authenticate_user!
 
   def create
   	if current_user.rol_id == 1 || current_user.rol_id == 2 
-	  	@fechadesde = params[:fechadesde]
-	  	@fechahasta = params[:fechahasta]
+	  	@fechadesde = params[:fechadesde].to_date.strftime("%Y-%m-%d")
+    	@fechahasta = params[:fechahasta].to_date.strftime("%Y-%m-%d")
 	    @libros = Repdescarga.where("fecha >= '#{@fechadesde}' and fecha <= '#{@fechahasta}'").group("title,libro_id,nombre_area,nombre_asignatura").select("title,libro_id,nombre_area,nombre_asignatura, COUNT (libro_id) as cantidad")
 	    #@libro = Vbuscalibronombre.joins("left join librodescargas on v_busca_libros_nombre.libro_id = librodescargas.libro_id").group("title,libro_id, nombre_area,nombre_asignatura").select("v_busca_libros_nombre.*, count(librodescargas.id) as cuenta_tweets")
 	    render :partial=> 'repdescarga'
