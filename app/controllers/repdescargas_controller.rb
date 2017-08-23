31f263740e4c6efd6007fe80adfa9b1f2909d3a7
Yefenny Chireno <yefenny300@gmail.com>
2017-08-23 03:26:45 -0400

    make it better

--- app/controllers/repdescargas_controller.rb
+++ app/controllers/repdescargas_controller.rb
@@ -5,8 +5,8 @@ class RepdescargasController < ApplicationController
   def create
   	@fechadesde = params[:fechadesde]
   	@fechahasta = params[:fechahasta]
-    @libros = Repdescarga.where("fecha >= '#{@fechadesde}' and fecha <= '#{@fechahasta}'").group("title,libro_id, nombre_area,nombre_asignatura").select("title,libro_id,nombre_area,nombre_asignatura, COUNT (libro_id) as cantidad")
-    @libro = Vbuscalibronombre.joins("left join librodescargas on v_busca_libros_nombre.libro_id = librodescargas.libro_id").group("title,libro_id, nombre_area,nombre_asignatura").select("v_busca_libros_nombre.*, count(librodescargas.id) as cuenta_tweets")
+    @libros = Repdescarga.where("fecha >= '#{@fechadesde}' and fecha <= '#{@fechahasta}'").group("title,libro_id,nombre_area,nombre_asignatura").select("title,libro_id,nombre_area,nombre_asignatura, COUNT (libro_id) as cantidad")
+    #@libro = Vbuscalibronombre.joins("left join librodescargas on v_busca_libros_nombre.libro_id = librodescargas.libro_id").group("title,libro_id, nombre_area,nombre_asignatura").select("v_busca_libros_nombre.*, count(librodescargas.id) as cuenta_tweets")
     render :partial=> 'repdescarga'
   end
 end
