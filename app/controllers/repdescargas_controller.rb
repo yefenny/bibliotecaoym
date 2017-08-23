962d9ecd34beb2494da95f6663b677bedd9a996d
Yefenny Chireno <yefenny300@gmail.com>
2017-08-23 02:36:52 -0400

    make it better

--- app/controllers/repdescargas_controller.rb
+++ app/controllers/repdescargas_controller.rb
@@ -5,8 +5,7 @@ class RepdescargasController < ApplicationController
   def create
   	@fechadesde = params[:fechadesde]
   	@fechahasta = params[:fechahasta]
-  	@libros = ActiveRecord::Base.connection.execute("SELECT title,libro_id,nombre_area,nombre_asignatura, COUNT (libro_id)FROM v_descargas where fecha >= '#{@fechadesde}' and fecha <= '#{@fechahasta}' GROUP BY
-    title,libro_id, nombre_area,nombre_asignatura")
+    @libros = Repdescarga.where("fecha >= '#{@fechadesde}' and fecha <= '#{@fechahasta}'").group("title,libro_id, nombre_area,nombre_asignatura").select("title,libro_id,nombre_area,nombre_asignatura, COUNT (libro_id) as cantidad")
     render :partial=> 'repdescarga'
   end
 end
