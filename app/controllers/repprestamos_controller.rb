e7a0b7a4b91c9bd4117b4bc8ad35484e5728e36b
Yefenny Chireno <yefenny300@gmail.com>
2017-08-22 21:07:53 -0400

    make it better

--- app/controllers/repprestamos_controller.rb
+++ app/controllers/repprestamos_controller.rb
@@ -3,4 +3,34 @@ class RepprestamosController < ApplicationController
   	@buscarl = "buscar"
   	@libros = Libro.order("title").all
   end
+  def create
+  	@buscarpor = params[:group1]
+  	@fechadesde = params[:fechadesde]
+  	@fechahasta = params[:fechahasta]
+  	@fechaahora = Time.now.strftime("%Y-%m-%d")
+  	@estudiante_matricula = params[:estudiante_matricula]
+  	@libro_id = params[:libro_id]
+  	sql = ""
+  	if @buscarpor == "activos"
+  		sql +="fechad IS NULL and fechae >= '#{fechaahora}' "
+  	elsif @buscarpor =="todos"	
+  		sql +="fechap = fechap"
+  	elsif @buscarpor =="devueltos"	
+  		sql +="fechad is not NULL"
+  	elsif @buscarpor =="vencidos"
+  		sql +="fechad IS NULL and fechae < '#{fechaahora}' "		
+  	end 
+  	if @estudiante_matricula != ""
+  		sql+= " and matricula ilike '#{@estudiante_matricula}'"
+  	end	
+  	if @libro_id != ""
+  		sql+= " and libro_id = #{@libro_id}"
+  	end	
+  	if @fechadesde !="" && @fechahasta !=""
+  		sql+= " and fechap >= '#{@fechadesde}' and fechap <= '#{@fechahasta}'"
+  	end	
+
+  	@rep = V_prestamo.where(sql).all
+  	render :partial=> 'repprestamo'
+  end
 end
