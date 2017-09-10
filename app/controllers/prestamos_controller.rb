30f43919109d0587caee429c991dd1d6a0e30efd
Yefenny Chireno <yefenny300@gmail.com>
2017-09-10 14:23:46 -0400

    make it better

--- app/controllers/prestamos_controller.rb
+++ app/controllers/prestamos_controller.rb
@@ -17,7 +17,7 @@ before_action :authenticate_user!
         @dias = (@prestamo.fechae.to_s.gsub("-", "").to_i - fechahoy.gsub("-", "").to_i).to_i
         puts "estos son los diaaaaaaaaas #{@dias} "
         if fechahoy.to_date > @prestamo.fechae.to_date
-          @dias = '14'
+          @prestamo.mora_id = 5
         end  
           respond_to do |format|
             format.html
